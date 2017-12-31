7163859bd51b0923cdff36254a6a7d931d6ce020
ikeyy2000@yahoo.com <Ikeyy2000@yahoo.com>
2017-12-31 03:12:43 +0100

    mobile menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,30 @@
 
-var abrev = function(options) {
+// This function shortens post text to 150 characters
+var shortenText = function(options) {
 
     options = $.extend({
         length: 150,
-        contn: ' ...',
+        cont: ' ...',
     }, options);
 
     $('.post-text').each(function() {
 
         var $post = $(this);
         var abrev = $post.text();
-        var shorten = abrev.substring(0, options.length) + options.contn;
-        $post.data('shorttext', shorten);
-        $post.data('fulltext', abrev);
+        var shorten = abrev.substring(0, options.length) + options.cont;
+        $post.data(shorten);
+        $post.data(abrev);
         $post.text(shorten);
     });
 };
 
 $(function() {
-	abrev();
+	shortenText();
 });
 
 
 
-// This function detects and adds and the .currentPage css styling to current page
+// This function detects and adds and the css styling of the current class to an active page
 $(function(){
   $('.active').each(function() {
     if ($(this).prop('href') == window.location.href) {
@@ -35,12 +36,22 @@ $(function(){
 
 
 // Mobile menu displays when the categories link is clicked
-var navButton = document.querySelector(".mobile-menu-button");
+$(function(){
 
-navButton.addEventListener("click", function (e) {
-  e.preventDefault();
+  var navButton = $('.mobile-menu-button')
 
-  // toggle nav state
-  document.body.classList.toggle("nav-visible");
+  navButton.on('click', function(e) {
+    e.preventDefault();
+    // toggle nav state
+    $('body').toggleClass('nav-visible')
+  });
+}); 
+
+// Mobile menu is closed when the X is clicked
+$(function(){
+
+  $('.close-menu').on('click', function(e){
+    $('body').toggleClass('nav-visible')
+  });
+
 });
-
